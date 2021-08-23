a403cc8bfdd3bb9eec87c5d13d6ea55141f34d46
Vaishnavi Gundla <vaishnavigundla2@gmail.com>
2021-08-23 18:30:31 +0530

    first code commit

---
+++ family.js
@@ -0,0 +1,71 @@
+var tree = {
+    "couple": {
+        "c1": {
+            "name": "Shankar Gundla",
+            "dob": "1948",
+            "imageUrl": "./assests/images/dada.png"
+        },
+        "c2": {
+            "name": "Laxmi Shankar Gundla",
+            "dob": "6th-Jan-1958",
+            "imageUrl": "./assests/images/nani.png"
+        },
+        "children": [
+            {
+                "c1": {
+                    "name": "Srinivas Gundla",
+                    "dob": "10-12-1977",
+                    "imageUrl": "./assests/images/papa.png"
+                },
+                "c2": {
+                    "name": "Anuradha Srinivas Gundla",
+                    "dob": "29-feb-1984",
+                    "imageUrl": "./assests/images/momo.png"
+                }
+            },
+            {
+                "c1": {
+                    "name": "satyanarayana Tumma",
+                    "dob": "17-september-1969",
+                    "imageUrl": "./assests/images/mama1.jpeg"
+                },
+                "c2": {
+                    "name": "uppa Tumma",
+                    "dob": "22-August-1979",
+                    "imageUrl": "./assests/images/uppa.jpeg"
+                }
+            },
+            {
+                "c1": {
+                    "name": "Dhanesh Gundla",
+                    "dob": "11-November-1985",
+                    "imageUrl": "./assests/images/chachu2.jpeg"
+                },
+                "c2": {
+                    "name": "Vandita Gundla",
+                    "dob": "25-January-1987",
+                    "imageUrl": "./assests/images/chachi.jpeg"
+                }
+            },
+            {
+                "c1": {
+                    "name": "Mohan Gundla",
+                    "dob": "13-February-1989",
+                    "imageUrl": "./assests/images/chachu3.jpeg"
+                }
+            },
+            {
+                "c1": {
+                    "name": "Saiprasad Ega",
+                    "dob": "21-April-1987",
+                    "imageUrl": "./assests/images/dhana.jpeg"
+                },
+                "c2": {
+                    "name": "Dhanalaxmi Ega",
+                    "dob": "27-April-1992",
+                    "imageUrl": "./assests/images/mama2.jpeg"
+                }
+            }
+        ]
+    }
+}
